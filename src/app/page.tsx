76e915c6bf632a5e7eeb4035ac89ec74a4f2498a
Gil Lohner <gil@lohner.net>
2025-04-06 22:17:57 +0200

    rem debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,28 @@ import Link from '@mui/material/Link';
 import NextLink from 'next/link';
 import ProTip from '@/components/ProTip';
 import Copyright from '@/components/Copyright';
-import { RequireLogin } from '@/components/NostrLogin';
 
 export default function Home() {
   return (
-    <RequireLogin>
-      <Container maxWidth="lg">
-        <Box
-          sx={{
-            my: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-            Material UI - Next.js App Router example in TypeScript
-          </Typography>
-          <Link href="/about" color="secondary" component={NextLink}>
-            Go to the about page
-          </Link>
-          <ProTip />
-          <Copyright />
-        </Box>
-      </Container>
-    </RequireLogin>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI - Next.js App Router example in TypeScript
+        </Typography>
+        <Link href="/about" color="secondary" component={NextLink}>
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
