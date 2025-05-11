36bed62f9cc3e067e54ec73b7b24d96449445b9d
Gil Lohner <gil@lohner.net>
2025-05-11 21:51:56 +0200

    eventcreationtemplate

--- src/components/common/events/CreateNewEventDialog/CreateNewEventDialog.tsx
+++ src/components/common/events/CreateNewEventDialog/CreateNewEventDialog.tsx
@@ -1,70 +1,266 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import Grid from "@mui/material/Grid";
 import { useTranslation } from "react-i18next";
+import InputAdornment from "@mui/material/InputAdornment";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 
-export default function CreateNewEventDialog({ event }) {
+// Import icons
+import EventIcon from "@mui/icons-material/Event";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import DescriptionIcon from "@mui/icons-material/Description";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PublicIcon from "@mui/icons-material/Public";
+import ImageIcon from "@mui/icons-material/Image";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export default function EventDialogTemplate() {
   const { t } = useTranslation();
   const [open, setOpen] = React.useState(false);
+  const [timezone, setTimezone] = React.useState(dayjs.tz.guess());
+  const [preview, setPreview] = React.useState("");
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleFileChange = (event) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => setPreview(e.target?.result);
+      reader.readAsDataURL(file);
+    }
   };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <React.Fragment>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
+    <>
+      <Button variant="outlined" onClick={() => setOpen(true)}>
+        Create Event
       </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        slotProps={{
-          paper: {
-            component: "form",
-            onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-              event.preventDefault();
-              const formData = new FormData(event.currentTarget);
-              const formJson = Object.fromEntries((formData as any).entries());
-              const email = formJson.email;
-              console.log(email);
-              handleClose();
-            },
-          },
-        }}
-      >
-        <DialogTitle>{t("event.createEvent.title")}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+      <Dialog open={open} onClose={() => setOpen(false)} maxWidth="md" fullWidth>
+        <DialogTitle sx={{ bgcolor: "background.default", color: "text.primary" }}>
+          <Typography variant="h5" component="div">
+            <EventIcon sx={{ mr: 1, verticalAlign: "middle", color: "primary.main" }} />
+            New Event Details
+          </Typography>
+        </DialogTitle>
+        <DialogContent sx={{ bgcolor: "background.paper" }}>
+          <form>
+            <Grid container spacing={2} direction="row" sx={{ marginTop: 1 }}>
+              {/* Left Column */}
+              <Grid size={{ xs: 12, md: 6 }}>
+                {" "}
+                <Grid container spacing={2} direction="column">
+                  {/* Title */}
+                  <Grid size={12}>
+                    <TextField
+                      fullWidth
+                      label="Event Title"
+                      name="title"
+                      required
+                      sx={{
+                        "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                        "& .MuiInputLabel-root": { color: "text.primary" },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <EventIcon color="primary" />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Grid>
+
+                  {/* Description */}
+                  <Grid item size={12}>
+                    <TextField
+                      fullWidth
+                      multiline
+                      minRows={4}
+                      label="Description"
+                      name="description"
+                      sx={{
+                        "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                        "& .MuiInputLabel-root": { color: "text.primary" },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <DescriptionIcon color="primary" />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              {/* Right Column */}
+              <Grid size={{ xs: 12, md: 6 }}>
+                {" "}
+                <Grid container spacing={2.5} direction="column">
+                  {/* Image Upload */}
+                  <Grid item>
+                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                      <Button
+                        component="label"
+                        variant="outlined"
+                        fullWidth
+                        startIcon={<ImageIcon color="primary" />}
+                        sx={{ mb: 2, color: "primary.main", borderColor: "primary.main" }}
+                      >
+                        Upload Image
+                        <input type="file" accept="image/*" hidden onChange={handleFileChange} />
+                      </Button>
+                      {preview && (
+                        <Paper
+                          elevation={2}
+                          sx={{ p: 1, width: "100%", bgcolor: "background.default" }}
+                        >
+                          <img
+                            src={preview}
+                            alt="Event preview"
+                            style={{ width: "100%", height: "auto", borderRadius: "4px" }}
+                          />
+                        </Paper>
+                      )}
+                    </Box>
+                  </Grid>
+
+                  {/* Location */}
+                  <Grid item>
+                    <TextField
+                      fullWidth
+                      label="Location"
+                      name="location"
+                      autoComplete="off"
+                      sx={{
+                        "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                        "& .MuiInputLabel-root": { color: "text.primary" },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <LocationOnIcon color="primary" />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              {/* Date/Time Section */}
+              <Grid size={12}>
+                <Paper elevation={0} sx={{ p: 2, bgcolor: "black" }}>
+                  <Typography variant="subtitle1" gutterBottom color="text.primary">
+                    Date and Time
+                  </Typography>
+                  <Grid container spacing={2} direction="row">
+                    <Grid size={{ xs: 12, md: 4 }}>
+                      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={timezone}>
+                        <DateTimePicker
+                          label="Start Time"
+                          slotProps={{
+                            textField: {
+                              fullWidth: true,
+                              sx: {
+                                "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                "& .MuiInputLabel-root": { color: "text.primary" },
+                              },
+                              InputProps: {
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <AccessTimeIcon color="primary" />
+                                  </InputAdornment>
+                                ),
+                              },
+                            },
+                          }}
+                        />
+                      </LocalizationProvider>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={timezone}>
+                        <DateTimePicker
+                          label="End Time"
+                          slotProps={{
+                            textField: {
+                              fullWidth: true,
+                              sx: {
+                                "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                "& .MuiInputLabel-root": { color: "text.primary" },
+                              },
+                              InputProps: {
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <AccessTimeIcon color="primary" />
+                                  </InputAdornment>
+                                ),
+                              },
+                            },
+                          }}
+                        />
+                      </LocalizationProvider>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                      <TextField
+                        select
+                        fullWidth
+                        label={t("event.createEvent.form.timezone")}
+                        value={timezone}
+                        onChange={(e) => setTimezone(e.target.value)}
+                        sx={{
+                          "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                          "& .MuiInputLabel-root": { color: "text.primary" },
+                        }}
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <PublicIcon color="primary" />
+                            </InputAdornment>
+                          ),
+                        }}
+                      >
+                        {Intl.supportedValuesOf("timeZone").map((tz) => (
+                          <MenuItem key={tz} value={tz}>
+                            {tz}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+            </Grid>
+
+            <Divider sx={{ my: 3, borderColor: "divider" }} />
+
+            <DialogActions sx={{ bgcolor: "background.default" }}>
+              <Button onClick={() => setOpen(false)} sx={{ color: "text.primary" }}>
+                Cancel
+              </Button>
+              <Button type="submit" variant="contained" color="primary">
+                Create
+              </Button>
+            </DialogActions>
+          </form>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
-        </DialogActions>
       </Dialog>
-    </React.Fragment>
+    </>
   );
 }
--- src/components/common/events/CreateNewEventDialog/dist/CreateNewEventDialog.js
+++ src/components/common/events/CreateNewEventDialog/dist/CreateNewEventDialog.js
@@ -2,44 +2,140 @@
 exports.__esModule = true;
 var React = require("react");
 var Button_1 = require("@mui/material/Button");
-var TextField_1 = require("@mui/material/TextField");
 var Dialog_1 = require("@mui/material/Dialog");
 var DialogActions_1 = require("@mui/material/DialogActions");
 var DialogContent_1 = require("@mui/material/DialogContent");
-var DialogContentText_1 = require("@mui/material/DialogContentText");
 var DialogTitle_1 = require("@mui/material/DialogTitle");
+var TextField_1 = require("@mui/material/TextField");
+var MenuItem_1 = require("@mui/material/MenuItem");
+var LocalizationProvider_1 = require("@mui/x-date-pickers/LocalizationProvider");
+var AdapterDayjs_1 = require("@mui/x-date-pickers/AdapterDayjs");
+var DateTimePicker_1 = require("@mui/x-date-pickers/DateTimePicker");
+var dayjs_1 = require("dayjs");
+var utc_1 = require("dayjs/plugin/utc");
+var timezone_1 = require("dayjs/plugin/timezone");
+var Grid_1 = require("@mui/material/Grid");
 var react_i18next_1 = require("react-i18next");
-function CreateNewEventDialog(_a) {
-    var event = _a.event;
+var InputAdornment_1 = require("@mui/material/InputAdornment");
+var Box_1 = require("@mui/material/Box");
+var Paper_1 = require("@mui/material/Paper");
+var Divider_1 = require("@mui/material/Divider");
+var Typography_1 = require("@mui/material/Typography");
+// Import icons
+var Event_1 = require("@mui/icons-material/Event");
+var LocationOn_1 = require("@mui/icons-material/LocationOn");
+var Description_1 = require("@mui/icons-material/Description");
+var AccessTime_1 = require("@mui/icons-material/AccessTime");
+var Public_1 = require("@mui/icons-material/Public");
+var Image_1 = require("@mui/icons-material/Image");
+dayjs_1["default"].extend(utc_1["default"]);
+dayjs_1["default"].extend(timezone_1["default"]);
+function EventDialogTemplate() {
     var t = react_i18next_1.useTranslation().t;
-    var _b = React.useState(false), open = _b[0], setOpen = _b[1];
-    var handleClickOpen = function () {
-        setOpen(true);
-    };
-    var handleClose = function () {
-        setOpen(false);
+    var _a = React.useState(false), open = _a[0], setOpen = _a[1];
+    var _b = React.useState(dayjs_1["default"].tz.guess()), timezone = _b[0], setTimezone = _b[1];
+    var _c = React.useState(""), preview = _c[0], setPreview = _c[1];
+    var handleFileChange = function (event) {
+        var _a;
+        var file = (_a = event.target.files) === null || _a === void 0 ? void 0 : _a[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) { var _a; return setPreview((_a = e.target) === null || _a === void 0 ? void 0 : _a.result); };
+            reader.readAsDataURL(file);
+        }
     };
     return (React.createElement(React.Fragment, null,
-        React.createElement(Button_1["default"], { variant: "outlined", onClick: handleClickOpen }, "Open form dialog"),
-        React.createElement(Dialog_1["default"], { open: open, onClose: handleClose, slotProps: {
-                paper: {
-                    component: "form",
-                    onSubmit: function (event) {
-                        event.preventDefault();
-                        var formData = new FormData(event.currentTarget);
-                        var formJson = Object.fromEntries(formData.entries());
-                        var email = formJson.email;
-                        console.log(email);
-                        handleClose();
-                    }
-                }
-            } },
-            React.createElement(DialogTitle_1["default"], null, t("event.createEvent.title")),
-            React.createElement(DialogContent_1["default"], null,
-                React.createElement(DialogContentText_1["default"], null, "To subscribe to this website, please enter your email address here. We will send updates occasionally."),
-                React.createElement(TextField_1["default"], { autoFocus: true, required: true, margin: "dense", id: "name", name: "email", label: "Email Address", type: "email", fullWidth: true, variant: "standard" })),
-            React.createElement(DialogActions_1["default"], null,
-                React.createElement(Button_1["default"], { onClick: handleClose }, "Cancel"),
-                React.createElement(Button_1["default"], { type: "submit" }, "Subscribe")))));
+        React.createElement(Button_1["default"], { variant: "outlined", onClick: function () { return setOpen(true); } }, "Create Event"),
+        React.createElement(Dialog_1["default"], { open: open, onClose: function () { return setOpen(false); }, maxWidth: "md", fullWidth: true },
+            React.createElement(DialogTitle_1["default"], { sx: { bgcolor: "background.default", color: "text.primary" } },
+                React.createElement(Typography_1["default"], { variant: "h5", component: "div" },
+                    React.createElement(Event_1["default"], { sx: { mr: 1, verticalAlign: "middle", color: "primary.main" } }),
+                    "New Event Details")),
+            React.createElement(DialogContent_1["default"], { sx: { bgcolor: "background.paper" } },
+                React.createElement("form", null,
+                    React.createElement(Grid_1["default"], { container: true, spacing: 2, direction: "row", sx: { marginTop: 1 } },
+                        React.createElement(Grid_1["default"], { size: { xs: 12, md: 6 } },
+                            " ",
+                            React.createElement(Grid_1["default"], { container: true, spacing: 2, direction: "column" },
+                                React.createElement(Grid_1["default"], { size: 12 },
+                                    React.createElement(TextField_1["default"], { fullWidth: true, label: "Event Title", name: "title", required: true, sx: {
+                                            "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                            "& .MuiInputLabel-root": { color: "text.primary" }
+                                        }, InputProps: {
+                                            startAdornment: (React.createElement(InputAdornment_1["default"], { position: "start" },
+                                                React.createElement(Event_1["default"], { color: "primary" })))
+                                        } })),
+                                React.createElement(Grid_1["default"], { item: true, size: 12 },
+                                    React.createElement(TextField_1["default"], { fullWidth: true, multiline: true, minRows: 4, label: "Description", name: "description", sx: {
+                                            "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                            "& .MuiInputLabel-root": { color: "text.primary" }
+                                        }, InputProps: {
+                                            startAdornment: (React.createElement(InputAdornment_1["default"], { position: "start" },
+                                                React.createElement(Description_1["default"], { color: "primary" })))
+                                        } })))),
+                        React.createElement(Grid_1["default"], { size: { xs: 12, md: 6 } },
+                            " ",
+                            React.createElement(Grid_1["default"], { container: true, spacing: 2.5, direction: "column" },
+                                React.createElement(Grid_1["default"], { item: true },
+                                    React.createElement(Box_1["default"], { sx: { display: "flex", flexDirection: "column", alignItems: "center" } },
+                                        React.createElement(Button_1["default"], { component: "label", variant: "outlined", fullWidth: true, startIcon: React.createElement(Image_1["default"], { color: "primary" }), sx: { mb: 2, color: "primary.main", borderColor: "primary.main" } },
+                                            "Upload Image",
+                                            React.createElement("input", { type: "file", accept: "image/*", hidden: true, onChange: handleFileChange })),
+                                        preview && (React.createElement(Paper_1["default"], { elevation: 2, sx: { p: 1, width: "100%", bgcolor: "background.default" } },
+                                            React.createElement("img", { src: preview, alt: "Event preview", style: { width: "100%", height: "auto", borderRadius: "4px" } }))))),
+                                React.createElement(Grid_1["default"], { item: true },
+                                    React.createElement(TextField_1["default"], { fullWidth: true, label: "Location", name: "location", autoComplete: "off", sx: {
+                                            "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                            "& .MuiInputLabel-root": { color: "text.primary" }
+                                        }, InputProps: {
+                                            startAdornment: (React.createElement(InputAdornment_1["default"], { position: "start" },
+                                                React.createElement(LocationOn_1["default"], { color: "primary" })))
+                                        } })))),
+                        React.createElement(Grid_1["default"], { size: 12 },
+                            React.createElement(Paper_1["default"], { elevation: 0, sx: { p: 2, bgcolor: "black" } },
+                                React.createElement(Typography_1["default"], { variant: "subtitle1", gutterBottom: true, color: "text.primary" }, "Date and Time"),
+                                React.createElement(Grid_1["default"], { container: true, spacing: 2, direction: "row" },
+                                    React.createElement(Grid_1["default"], { size: { xs: 12, md: 4 } },
+                                        React.createElement(LocalizationProvider_1.LocalizationProvider, { dateAdapter: AdapterDayjs_1.AdapterDayjs, adapterLocale: timezone },
+                                            React.createElement(DateTimePicker_1.DateTimePicker, { label: "Start Time", slotProps: {
+                                                    textField: {
+                                                        fullWidth: true,
+                                                        sx: {
+                                                            "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                                            "& .MuiInputLabel-root": { color: "text.primary" }
+                                                        },
+                                                        InputProps: {
+                                                            startAdornment: (React.createElement(InputAdornment_1["default"], { position: "start" },
+                                                                React.createElement(AccessTime_1["default"], { color: "primary" })))
+                                                        }
+                                                    }
+                                                } }))),
+                                    React.createElement(Grid_1["default"], { size: { xs: 12, md: 4 } },
+                                        React.createElement(LocalizationProvider_1.LocalizationProvider, { dateAdapter: AdapterDayjs_1.AdapterDayjs, adapterLocale: timezone },
+                                            React.createElement(DateTimePicker_1.DateTimePicker, { label: "End Time", slotProps: {
+                                                    textField: {
+                                                        fullWidth: true,
+                                                        sx: {
+                                                            "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                                            "& .MuiInputLabel-root": { color: "text.primary" }
+                                                        },
+                                                        InputProps: {
+                                                            startAdornment: (React.createElement(InputAdornment_1["default"], { position: "start" },
+                                                                React.createElement(AccessTime_1["default"], { color: "primary" })))
+                                                        }
+                                                    }
+                                                } }))),
+                                    React.createElement(Grid_1["default"], { size: { xs: 12, md: 4 } },
+                                        React.createElement(TextField_1["default"], { select: true, fullWidth: true, label: t("event.createEvent.form.timezone"), value: timezone, onChange: function (e) { return setTimezone(e.target.value); }, sx: {
+                                                "& .MuiInputBase-root": { bgcolor: "background.paper" },
+                                                "& .MuiInputLabel-root": { color: "text.primary" }
+                                            }, InputProps: {
+                                                startAdornment: (React.createElement(InputAdornment_1["default"], { position: "start" },
+                                                    React.createElement(Public_1["default"], { color: "primary" })))
+                                            } }, Intl.supportedValuesOf("timeZone").map(function (tz) { return (React.createElement(MenuItem_1["default"], { key: tz, value: tz }, tz)); }))))))),
+                    React.createElement(Divider_1["default"], { sx: { my: 3, borderColor: "divider" } }),
+                    React.createElement(DialogActions_1["default"], { sx: { bgcolor: "background.default" } },
+                        React.createElement(Button_1["default"], { onClick: function () { return setOpen(false); }, sx: { color: "text.primary" } }, "Cancel"),
+                        React.createElement(Button_1["default"], { type: "submit", variant: "contained", color: "primary" }, "Create")))))));
 }
-exports["default"] = CreateNewEventDialog;
+exports["default"] = EventDialogTemplate;
