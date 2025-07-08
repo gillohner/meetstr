e434ff40cfefd486b558538fdb8a9c8bbd8b83aa
Gil Lohner <gil@lohner.net>
2025-07-08 11:35:28 +0200

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "meetstr",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "/root/meetstr",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      restart_delay: 4000,
+    },
+  ],
+};
